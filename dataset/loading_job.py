e623916ca61b873b1a539f53d74dfb80a6389ee1
Noor Kowshik <kowshik.noor@gmail.com>
2022-04-04 16:55:56 -0400

    create loading jobs

--- dataset/loading_job.py
+++ dataset/loading_job.py
@@ -30,4 +30,39 @@ conn = tg.TigerGraphConnection(host=host_name,
     apiToken=auth_token)
 
 # listing vertex count requires graph authentication
-print(conn.getVertexCount("*"))
+# print(conn.getVertexCount("*"))
+
+conn.gsql('''
+USE GRAPH drugs
+CREATE LOADING JOB load_products FOR GRAPH drugs {
+    DEFINE FILENAME MyDataSource;
+    LOAD MyDataSource TO VERTEX Product VALUES(gsql_concat($1,",",$5), $5, $3, $4, $7) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO VERTEX Form VALUES($2) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO VERTEX ActiveIngredient VALUES($6) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_form VALUES(gsql_concat($1,",",$5), $2) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_active_ingredient VALUES(gsql_concat($1,",",$5), $6) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_application VALUES(gsql_concat($1,",",$5), $0) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+}
+CREATE LOADING JOB load_application_data FOR GRAPH drugs {
+    DEFINE FILENAME MyDataSource;
+    LOAD MyDataSource TO VERTEX Application VALUES($0, $2) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO VERTEX Sponsor VALUES($0, $3) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO VERTEX ApplicationType VALUES($0, $1) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_sponsor VALUES($0, $3) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_type VALUES($0, $1) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+}
+CREATE LOADING JOB load_te FOR GRAPH drugs {
+    DEFINE FILENAME MyDataSource;
+    LOAD MyDataSource TO VERTEX TE VALUES($3) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+    LOAD MyDataSource TO EDGE has_te VALUES($0, $3) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+}
+CREATE LOADING JOB load_marketing_lookup FOR GRAPH drugs {
+    DEFINE FILENAME MyDataSource;
+    LOAD MyDataSource TO VERTEX MarketingStatus VALUES($0, $1) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+}
+CREATE LOADING JOB load_marketing FOR GRAPH drugs {
+    DEFINE FILENAME MyDataSource;
+    LOAD MyDataSource TO EDGE has_marketing_status VALUES($1, $0) USING SEPARATOR="\t", HEADER="false", EOL="\n" QUOTE="none";
+}
+
+''')
