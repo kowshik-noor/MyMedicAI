7ffe2cf59a55ac5496c44cbdaf6dc972a5f1f672
Noor Kowshik <kowshik.noor@gmail.com>
2022-04-04 14:09:49 -0400

    graph authentication established

--- dataset/loading_job.py
+++ dataset/loading_job.py
@@ -11,5 +11,23 @@ password = "tigergraph"
 # establish connection to the TigerGraph solution
 conn = tg.TigerGraphConnection(host=host_name, username=user_name, password=password)
 
-# print the current schema to verify that i'm connected
-print(conn.gsql('LS'))
+# GRAPH CONNECTION
+# creating the secret and token that's needed to authenticate our graph
+
+# set the name of graph to connect to
+conn.graphname = graph_name
+
+# create a secret
+secret = conn.createSecret()
+# use the secret to get a token
+auth_token = conn.getToken(secret)[0]
+
+# connect to the graph with the token
+conn = tg.TigerGraphConnection(host=host_name, 
+    username=user_name, 
+    password=password, 
+    graphname=graph_name,
+    apiToken=auth_token)
+
+# listing vertex count requires graph authentication
+print(conn.getVertexCount("*"))
